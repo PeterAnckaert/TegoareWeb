051a37b09edb6e127aff1cf15c0c6e931d039d63
Peter Anckaert <r0780531@student.vives.be>
2021-03-13 09:50:31 +0100

    styling ontmoetingsplaats afgewerkt, styling relatie afgewerkt, styling buttons afgewerkt (zoek, edit, delete, terug, ...)

--- Data/KnopInfo.cs
+++ Data/KnopInfo.cs
@@ -7,9 +7,10 @@ namespace TegoareWeb.Data
 {
     public class KnopInfo
     {
-        public string Label { get; set; } = "Terug";
-        public string Controller { get; set; } = "";
+        public string Label { get; set; } = "Terug naar lijst";
+        public string Controller { get; set; }
         public string Action { get; set; } = "Index";
         public string CSS { get; set; } = "btn-dark";
+        public string FA { get; set; } = "fa-angle-double-left";
     }
 }
